bf08bda514e7d6b2bec10168692b653f5a67624b
imenBHamed <imen.benhamed123@gmail.com>
2018-10-02 17:37:58 -0400

    ajout patron de conception Singleton

---
+++ Singleton_DP/src/dp/Application.java
@@ -0,0 +1,12 @@
+package dp;
+
+public class Application {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++){
+			Singleton s = Singleton.getInstence();
+			s.traiter("T"+i);
+		}
+	}
+
+}
---
+++ Singleton_DP/src/dp/Singleton.java
@@ -0,0 +1,32 @@
+package dp;
+
+public class Singleton {
+ private static final Singleton singleton;
+ private int compteur;
+ 
+ static{
+	 singleton= new Singleton();
+ }
+  private Singleton(){
+	  System.out.println("Instanciation du singleton");
+  }
+  
+  public static Singleton getInstence(){
+	  return singleton;
+  }
+  
+  public void traiter(String taskName){
+	  System.out.println("...Traitement de la tâche"+ taskName);
+	  for(int i=1;i<=5;i++){
+		  ++compteur;
+		  System.out.println(".."+ compteur);
+		  try{
+			  Thread.sleep(1000);
+		  } catch (InterruptedException e){
+			  e.printStackTrace();
+		  }	  
+	  }
+	  System.out.println("Fin du traitement de la tâche"+ taskName+ "compteur="+compteur);
+
+  }
+}
